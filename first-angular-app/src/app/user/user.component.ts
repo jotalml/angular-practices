853cedd0e1bb1ab972459452f43b7d0a69cd75e3
Jose Martinez <joseluis.martinez.lameda.contractor@bbva.com>
2024-11-29 16:24:10 -0400

    add new first-app

---
+++ first-angular-app/src/app/user/user.component.ts
@@ -0,0 +1,42 @@
+import { Component, computed, EventEmitter, Input, input, output, Output, signal } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+import { type User } from './user.model';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+// type User = {
+//   id:string,
+//   name: string,
+//   avatar: string
+// }
+
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+
+  @Input({required: true}) user!: User;
+  @Input({required: true}) selected!: boolean;
+
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>();
+
+  get imagePath(){
+    return `assets/users/${this.user.avatar}`
+  }
+  // imagePath = computed(() => `assets/users/${this.avatar()}`);
+
+  onSelectUser(){
+    this.select.emit(this.user.id);
+  }
+  
+
+
+  
+
+}
