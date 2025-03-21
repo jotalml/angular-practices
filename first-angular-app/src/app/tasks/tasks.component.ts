20317572e936dae4c7951536312674d7271038e0
Jose Martinez <joseluis.martinez.lameda.contractor@bbva.com>
2025-03-21 16:06:30 -0400

    adding new functionalities

--- first-angular-app/src/app/tasks/tasks.component.ts
+++ first-angular-app/src/app/tasks/tasks.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { type NewTaskData } from './task/task.model';
 import { NewTaskComponent } from "./new-task/new-task.component";
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -14,46 +16,18 @@ export class TasksComponent {
   @Input({required:true}) name!: string;
   isAddingTask: boolean = false;
 
-  get selectedUserTasks() {
-    return this.dummyTasks.filter(task => task.userId === this.userId);
-  }
+  constructor(private tasksService:TasksService){}
 
-  onCompleteTask(id: string) {
-    this.dummyTasks = this.dummyTasks.filter(task => task.id !== id);
+  get selectedUserTasks() {
+    return this.tasksService.getUserTasks(this.userId);
   }
 
   onStartAddTask() {  
     this.isAddingTask = true;
   }
 
-  onCancelAddTask(){
+  onCloseAddTask(){
     this.isAddingTask = false;
   }
-
-  dummyTasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ]
 
 }
--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, computed, EventEmitter, Input, input, output, Output, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 import { type User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
@@ -14,7 +15,7 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
